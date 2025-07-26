775b27e5806c83aa4e528672b2cd8c37decbf43a
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-07-26 17:00:33 +0100

    implement sell logic for addOrder

--- engine/src/core/OrderBook.cpp
+++ engine/src/core/OrderBook.cpp
@@ -2,6 +2,7 @@
 
 bool OrderBook::addOrder(Order& order) 
 {
+    static uint32_t nextTradeId = 1;        // TODO: Improve this, probably not static
     switch (order.side) {
         case OrderSide::BUY:
             while (order.quantity > 0 && !asks.empty() && order.price >= asks.begin()->first) {
@@ -9,7 +10,6 @@ bool OrderBook::addOrder(Order& order)
                 while (order.quantity > 0 && !restingSellOrders.empty()) {
                     Order& restingOrder = restingSellOrders.front();
                     uint32_t tradeQuantity = std::min(order.quantity, restingOrder.quantity);
-                    static uint32_t nextTradeId = 1;        // TODO: Improve this, probably not static
                     Trade newTrade;
                     newTrade.tradeId = nextTradeId++;
                     newTrade.securityId = order.securityId;
@@ -41,8 +41,47 @@ bool OrderBook::addOrder(Order& order)
                         break;
                 }
             }
+            break;
         case OrderSide::SELL:
-            while ()
+            while (order.quantity > 0 && !bids.empty() && order.price <= bids.begin()->first) {
+                std::queue<Order>& restingBidOrders = bids.begin()->second;
+                while (order.quantity > 0 && !restingBidOrders.empty()) {
+                    Order& restingOrder = restingBidOrders.front();
+                    uint32_t tradeQuantity = std::min(order.quantity, restingOrder.quantity);
+                    Trade newTrade;
+                    newTrade.tradeId = nextTradeId++;
+                    newTrade.securityId = order.securityId;
+                    newTrade.price = restingOrder.price;            
+                    newTrade.quantity = tradeQuantity;
+                    newTrade.aggressingOrderId = order.orderId;
+                    newTrade.restingOrderId = restingOrder.orderId;
+                    newTrade.timestamp = std::chrono::duration_cast<std::chrono::nanoseconds> (std::chrono::system_clock::now().time_since_epoch()).count();
+
+                    order.quantity -= newTrade.quantity;
+                    restingOrder.quantity -= newTrade.quantity;
+                 
+                    if (restingOrder.quantity == 0)
+                        restingBidOrders.pop();
+                }
+
+                if (bids.begin()->second.empty())
+                    bids.erase(bids.begin());
+            }
+
+            // NOTE: could probably rewrite this core logic into a helper function instead
+
+            if (order.quantity != 0) {
+                switch (order.type) {
+                    case OrderType::LIMIT:
+                        asks[order.price].push(order);
+                        break;
+                    case OrderType::MARKET:
+                        return false;
+                    default:
+                        break;
+                }
+            }
+            break;
         default:
             break;
     }

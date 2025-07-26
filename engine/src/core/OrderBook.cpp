12a84c7fba9372ea1a6ccd20da8b75dab8960e78
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-07-26 10:04:51 +0100

    OrderBook::addOrder first version implemented

--- engine/src/core/OrderBook.cpp
+++ engine/src/core/OrderBook.cpp
@@ -1,47 +1,53 @@
 #include "OrderBook.h"
 
-bool OrderBook::addOrder(const Order& order) 
+bool OrderBook::addOrder(Order& order) 
 {
     switch (order.side) {
         case OrderSide::BUY:
             while (order.quantity > 0 && !asks.empty() && order.price >= asks.begin()->first) {
                 std::queue<Order>& restingSellOrders = asks.begin()->second;
-                for (; !restingSellOrders.empty(); restingSellOrders.pop()) {
+                while (order.quantity > 0 && !restingSellOrders.empty()) {
                     Order& restingOrder = restingSellOrders.front();
-                    uint32_t tradeQuantity = ;
+                    uint32_t tradeQuantity = std::min(order.quantity, restingOrder.quantity);
+                    static uint32_t nextTradeId = 1;        // TODO: Improve this, probably not static
                     Trade newTrade;
-                    newTrade.tradeId = 
-                    newTrade.securityId = 
-                    newTrade.price = order.price;
-                    newTrade.quantity = order.quantity;
-                    newTrade.aggressingOrderId =
-                    newTrade.restingOrderId = 
-                    newTrade.timestamp = 
+                    newTrade.tradeId = nextTradeId++;
+                    newTrade.securityId = order.securityId;
+                    newTrade.price = restingOrder.price;
+                    newTrade.quantity = tradeQuantity;
+                    newTrade.aggressingOrderId = order.orderId;
+                    newTrade.restingOrderId = restingOrder.orderId;
+                    newTrade.timestamp = std::chrono::duration_cast<std::chrono::nanoseconds> (std::chrono::system_clock::now().time_since_epoch()).count();
 
                     order.quantity -= newTrade.quantity;
                     restingOrder.quantity -= newTrade.quantity;
+                 
+                    if (restingOrder.quantity == 0)
+                        restingSellOrders.pop();
                 }
 
-                if (asks.begin().empty())
-                    asks.erase(order.price);
+                if (asks.begin()->second.empty())
+                    asks.erase(asks.begin());
             }
 
             if (order.quantity != 0) {
                 switch (order.type) {
                     case OrderType::LIMIT:
-                        bids.insert(order.price, order);
+                        bids[order.price].push(order);
+                        break;
                     case OrderType::MARKET:
-                        std::cout << "ORDER REJECTED ???? " << std::endl;
+                        return false;
                     default:
                         break;
                 }
             }
         case OrderSide::SELL:
-            // Sell
             break;
         default:
             break;
     }
+
+    return true;
 }
 
 
